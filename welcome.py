a7932393edd4fa49bd931ed46634233aecd46e4d
HuyNguyen <huy8208@gmail.com>
2021-01-13 23:35:52 -0800

    Rewrited welcome.py and sucesfully added sign_up and sign_in features

--- welcome.py
+++ welcome.py
@@ -2,45 +2,53 @@ from kivy.lang import Builder
 from kivymd.app import MDApp
 from kivy.core.window import Window
 from kivy.config import Config
-from kivy.uix.screenmanager import Screen,ScreenManager
-from kivy.uix.behaviors import ButtonBehavior 
-from kivy.uix.image import Image  
+from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.uix.behaviors import ButtonBehavior
+from kivy.uix.image import Image
 from kivymd.uix.dialog import MDDialog
 from kivymd.uix.button import MDFlatButton
+from kivy.network.urlrequest import UrlRequest
+import urllib.parse  # Encoder for UrlRequest
+import certifi  # Certificate for UrlRequest
+import json
 
-Config.set('graphics', 'resizable', False) 
-Window.size = (1920 ,1080)
+Config.set('graphics', 'resizable', False)
+Window.size = (1920, 1080)
 
 
 class WelcomeScreen(Screen):
     pass
 
+
 class SignInScreen(Screen):
     pass
+
 
 class CreateUserScreen(Screen):
     pass
 
-sm = ScreenManager()
-sm.add_widget(WelcomeScreen(name = 'welcomescreen'))
-sm.add_widget(SignInScreen(name = 'signinscreen'))
-sm.add_widget(CreateUserScreen(name = 'createuserscreen'))
 
+sm = ScreenManager()
+sm.add_widget(WelcomeScreen(name='welcomescreen'))
+sm.add_widget(SignInScreen(name='signinscreen'))
+sm.add_widget(CreateUserScreen(name='createuserscreen'))
 
 
 class FastAZ(MDApp):
     def __init__(self, **kwargs):
-        super().__init__(kwargs)
-        self.username_text = ""
-        self.password_text = ""
-        #HELLO THIS IS APPLE
+        """ Declares all variables in here """
+        super().__init__(**kwargs)
+        self.LOGIN_URL = 'http://fastaz.vn/wp-json/jwt-auth/v1/token'
+        self.CREATE_ACCOUNT_URL = 'http://fastaz.vn/'
+        self.login_state = False
+
     def build(self):
         self.theme_cls.primary_palette = 'Cyan'
         # self.theme_cls.theme_style = "Dark"
         self.theme_cls.primary_hue = "A700"
         self.strng = Builder.load_file('welcome.kv')
         return self.strng
-    
+
     def sign_up_screen(self):
         self.strng.current = 'createuserscreen'
         self.strng.transition.direction = 'left'
@@ -48,49 +56,129 @@ class FastAZ(MDApp):
     def sign_in_screen(self):
         self.strng.current = 'signinscreen'
         self.strng.transition.direction = 'left'
-    
+
     def create_user_database(self):
         # Các Biến Trong Input
-        self.username_create = self.strng.get_screen('createuserscreen').ids.username_create.text
-        self.password_create = self.strng.get_screen('createuserscreen').ids.password_create.text
-        self.email_create = self.strng.get_screen('createuserscreen').ids.email_create.text
-        self.name_create = self.strng.get_screen('createuserscreen').ids.name_create.text
-        self.phone_create = self.strng.get_screen('createuserscreen').ids.phone_create.text
-        #Tạo request tới Wordpress để tạo tài khoản rồi trả lại thông tin đã tạo tại khoản
+        username = self.strng.get_screen('createuserscreen').ids.username_create.text
+        password = self.strng.get_screen('createuserscreen').ids.password_create.text
+        email = self.strng.get_screen('createuserscreen').ids.email_create.text
+        name = self.strng.get_screen('createuserscreen').ids.name_create.text
+        phone = self.strng.get_screen('createuserscreen').ids.phone_create.text
 
-        cancel_btn_user_dialoue = MDFlatButton (text = 'Thử lại', on_release = self.close_username_dialoue)
-        self.dialog = MDDialog (title = 'Tạo tài khoản', text = f'Đã tạo Username : {self.username_create}. Password : {self.password_create}. Email : {self.email_create}', size_hint = (0.5 , 0.2), buttons = [cancel_btn_user_dialoue])
-        self.dialog.open()
+        params = urllib.parse.urlencode({'user_name': username,
+                                         'password': password,
+                                         'first_name': name,
+                                         'phone': phone,
+                                         'email': email,
+                                         'swpm_api_action': 'create',
+                                         'key': 'e3003c44deff96ebe7db442100f3b473'})
+        headers = {'Content-type': 'application/x-www-form-urlencoded',
+                   'Accept': 'text/plain'}
+
+        UrlRequest(self.CREATE_ACCOUNT_URL, req_body=params,
+                   on_success=self.create_account_validation,
+                   req_headers=headers,
+                   on_error=self.sign_in_error, ca_file=certifi.where())
+
+        # Tạo request tới Wordpress để tạo tài khoản rồi trả lại thông tin đã tạo tại khoản
+        # unsuccessful_sign_up_dialog = self.create_dialog('Tạo tài khoản'
+        #                                                  ,
+        #                                                  f'Đã tạo Username : {username}. Password : {password}. Email : {email}'
+        #                                                  , 'Thử lại', lambda a: unsuccessful_sign_up_dialog.dismiss())
+        # unsuccessful_sign_up_dialog.open()
+
+
+
     def sign_in_database(self):
         # Các Biến Trong Input
-        self.username_text = self.strng.get_screen('signinscreen').ids.username_text_field.text
-        self.password_text = self.strng.get_screen('signinscreen').ids.password_text_field.text
-        # Check Thông Tin có rỗng không
-        if self.username_text == '' or self.password_text == '':
-            cancel_btn_user_dialoue = MDFlatButton (text = 'Thử lại', on_release = self.close_username_dialoue)
-            self.dialog = MDDialog (title = 'Lỗi thiếu thông tin', text = 'Username và Password không được để trống', size_hint = (0.5 , 0.2), buttons = [cancel_btn_user_dialoue])
-            self.dialog.open()
+        username_text = self.strng.get_screen('signinscreen').ids.username_text_field.text
+        password_text = self.strng.get_screen('signinscreen').ids.password_text_field.text
+
+        if username_text == '' or password_text == '':
+            missing_info_dialog = self.create_dialog(dialog_title='Lỗi thiếu thông tin',
+                                                     dialog_text='Username và Password không được để trống',
+                                                     button_text='Thử lại'
+                                                     , button_on_release=lambda a: missing_info_dialog.dismiss())
+            missing_info_dialog.open()
+
+        print("Attempting to sign user in: ", username_text, password_text)
+        params = urllib.parse.urlencode({'username': username_text, 'password': password_text})
+        headers = {'Content-type': 'application/x-www-form-urlencoded',
+                   'Accept': 'text/plain'}
+        UrlRequest(self.LOGIN_URL, req_body=params,
+                   on_success=self.sign_in_validation,
+                   req_headers=headers,
+                   on_error=self.sign_in_error, ca_file=certifi.where())
+
+    def sign_in_validation(self, urlrequest, log_in_data):
+        sign_in_code_status = log_in_data['success']  # Boolean value
+        if sign_in_code_status:
+            print('Đã Đăng Nhập Thành Công')  # <------ Chưa thiết kế creen cho đăng nhập thành công
+            print(log_in_data)
+            # membershipLevel = int(log_in_data['member_data']['membership_level'])
+            # if membershipLevel == 2:
+            #     print('Đây là tài khoản Free')
+            # elif membershipLevel == 3:
+            #     print('Đây là tài khoản Vip')
         else:
-################################################################################
-# 
-#     CHECK TÀI KHOẢN TRÊN DATABASE RỒI TRẢ VỀ THÔNG TIN True hoặc False
-# 
-################################################################################  
-            self.x = self.check_user_database()
-            if self.x == True:
-                print ('Đã Đăng Nhập Thành Cồng') # <------ Chưa thiết kế creen cho đăng nhập thành công
-                
-            if self.x == False:
-                cancel_btn_user_dialoue = MDFlatButton (text = 'Nhập lại', on_release = self.close_username_dialoue)
-                self.dialog = MDDialog (title = 'Sai thông tin đăng nhập', text = 'Username hoặc Password không đúng', size_hint = (0.5 , 0.2), buttons = [cancel_btn_user_dialoue])
-                self.dialog.open()
-    # HÀM CHECK TÀI KHOẢN                
-    def check_user_database(self):
-        self.result = False  #Demo Trả Kết Quả
-        return self.result
+            print('Đăng nhập không thành công')
+            unsuccessful_login_dialog = self.create_dialog('Sai thông tin đăng nhập',
+                                                           'Username hoặc Password không đúng', 'Nhập lại',
+                                                           lambda a: unsuccessful_login_dialog.dismiss())
+            unsuccessful_login_dialog.open()
 
+    def sign_in_error(self, *args):
+        # Required by Urlrequest
+        print("Sign in error", args)
 
-    def close_username_dialoue(self,obj):
-        self.dialog.dismiss() 
+    def create_dialog(self, dialog_title: str, dialog_text: str, button_text: str, button_on_release) -> MDDialog:
+        """ Create a pop-up dialog with custom input parameters"""
+        dialog = MDDialog(title=dialog_title,
+                          text=dialog_text,
+                          size_hint=(0.5, 0.2), buttons=[MDFlatButton(text=button_text, on_release=button_on_release)])
+        return dialog
+
+    def clear_user_sign_up_fields(self):
+        # Clear text fields
+        self.strng.get_screen('createuserscreen').ids.username_create.text = ''
+        self.strng.get_screen('createuserscreen').ids.password_create.text = ''
+        self.strng.get_screen('createuserscreen').ids.email_create.text = ''
+        self.strng.get_screen('createuserscreen').ids.name_create.text = ''
+        self.strng.get_screen('createuserscreen').ids.phone_create.text = ''
+    
+
+    def create_account_validation(self,urlrequest, sign_up_data):
+        sign_up_result = json.loads(sign_up_data)
+        print(sign_up_result)
+        if sign_up_result['result'] == 'success':
+            print("Create account successfully")
+            self.clear_user_sign_up_fields()
+            self.strng.current = 'signinscreen'
+        elif sign_up_result['result'] == 'failure' and 'email' in sign_up_result['errors']:  # Both username and email already
+            # existed error!
+            print("Fail to create account")
+            sign_up_failure_dialog = self.create_dialog('Tạo account không thành không'
+                                                        ,"Username ho email đã được đăng ký. Bạn hãy thử đăng ký lại "
+                                                         "nhé. "
+                                                        ,'Thử Lại '
+                                                        ,lambda a:sign_up_failure_dialog.dismiss())
+            sign_up_failure_dialog.open()
+        elif sign_up_result['result'] == 'failure':
+            print("Fail to create account")
+            sign_up_failure_dialog = self.create_dialog('Tạo account không thành không'
+                                                        ,"Email đã được đăng ký, bạn hãy thử đăng nhập nhé.","Thử Lại"
+                                                        ,lambda a:sign_up_failure_dialog.dismiss())
+            sign_up_failure_dialog.open()
+        else:
+            print("Unidentified Sgn Up Error !!!")
+    def retrieve_user_config(self):
+        # check if user already had setting, if so, pull config from mongoDB
+        # Else create user setting
+        pass
+
+    def update_user_config(self):
+        # Update user config everytime user quits or manually update setting in dashboard.
+        pass
+
 
 FastAZ().run()
