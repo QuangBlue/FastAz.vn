d91f0db503001e26effc19a34c206d3b6407fe8e
QuangBlue <quangblue1603@gmail.com>
2021-01-14 22:53:38 +0700

    Update code dashboard screen

--- welcome.py
+++ welcome.py
@@ -14,24 +14,40 @@ import json
 
 Config.set('graphics', 'resizable', False)
 Window.size = (1920, 1080)
-
-
+# TẠO SCREEN CẦN 3 BƯỚC
+# BƯỚC 1 TẠO CLASS CHO SCREEN
 class WelcomeScreen(Screen):
     pass
-
 
 class SignInScreen(Screen):
     pass
 
-
 class CreateUserScreen(Screen):
     pass
 
+class DashBoardScreen(Screen):
+    pass
 
+class Screen_1(Screen):
+    pass
+
+class Screen_2(Screen):
+    pass
+
+class Screen_3(Screen):
+    pass
+
+class Screen_4(Screen):
+    pass
+
+# BƯỚC 2 ADD CLASS CHO SCREEN VÀO SCREEN MANAGER
+# BƯỚC 3 ADD VÀO FILE KV
 sm = ScreenManager()
-sm.add_widget(WelcomeScreen(name='welcomescreen'))
-sm.add_widget(SignInScreen(name='signinscreen'))
-sm.add_widget(CreateUserScreen(name='createuserscreen'))
+sm.add_widget(WelcomeScreen(name = 'welcomescreen'))
+sm.add_widget(SignInScreen(name = 'signinscreen'))
+sm.add_widget(CreateUserScreen(name = 'createuserscreen'))
+sm.add_widget(DashBoardScreen(name = 'dashboardscreen'))
+
 
 
 class FastAZ(MDApp):
@@ -115,6 +131,8 @@ class FastAZ(MDApp):
         if sign_in_code_status:
             print('Đã Đăng Nhập Thành Công')  # <------ Chưa thiết kế creen cho đăng nhập thành công
             print(log_in_data)
+            self.strng.current = 'dashboardscreen'
+            self.strng.transition.direction = 'left'             
             # membershipLevel = int(log_in_data['member_data']['membership_level'])
             # if membershipLevel == 2:
             #     print('Đây là tài khoản Free')
@@ -179,6 +197,24 @@ class FastAZ(MDApp):
     def update_user_config(self):
         # Update user config everytime user quits or manually update setting in dashboard.
         pass
+    
+    # Create Effec Button DashBoard Screen
+    def button_change_color(self,x):
+        l = ['bt_dashboard' , 'bt_boot_items' , 'bt_reply_ratings' , 'bt_setting']
+        for k in l:
+            if x == k :
+                r = exec(f"self.strng.get_screen('dashboardscreen').ids.{x}.md_bg_color = (255/255, 255/255, 255/255, 1)")
+                r = exec(f"self.strng.get_screen('dashboardscreen').ids.{x}.text_color = (10/255, 10/255, 10/255, 1)")
+                r = exec(f"self.strng.get_screen('dashboardscreen').ids.{x}.icon_color = (10/255, 10/255, 10/255, 1)")
+                for i in l:
+                    if i == x:
+                        continue
+                    else:
+                        exec(f"self.strng.get_screen('dashboardscreen').ids.{i}.text_color = (255/255, 255/255, 255/255, 1)")
+                        exec(f"self.strng.get_screen('dashboardscreen').ids.{i}.icon_color = (255/255, 255/255, 255/255, 1)")
+                        exec(f"self.strng.get_screen('dashboardscreen').ids.{i}.md_bg_color = (40/255, 40/255, 40/255, 1)")
+                break
+
 
 
 FastAZ().run()
