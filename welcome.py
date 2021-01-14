44bfd005c6a80f24ad8a83f8acfd41de1a800d3a
QuangBlue <quangblue1603@gmail.com>
2021-01-14 23:10:07 +0700

    Commit of QuangBlue

--- welcome.py
+++ welcome.py
@@ -215,6 +215,6 @@ class FastAZ(MDApp):
                         exec(f"self.strng.get_screen('dashboardscreen').ids.{i}.md_bg_color = (40/255, 40/255, 40/255, 1)")
                 break
 
-
+#THIS IS COMMENT OF QUANGBLUE
 
 FastAZ().run()
