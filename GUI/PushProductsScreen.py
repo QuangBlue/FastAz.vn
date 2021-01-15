1d97cc504df589b66a2c25e946732836a0f6501a
huy8208 <43127420+huy8208@users.noreply.github.com>
2021-01-14 16:35:26 -0800

    Delete GUI directory

--- GUI/PushProductsScreen.py
+++
@@ -1,8 +0,0 @@
-from kivy.uix.screenmanager import Screen, SlideTransition
-
-
-class PushProductsScreen(Screen):
-    def testing123(self):
-        self.parent.transition = SlideTransition(direction="right")
-        self.parent.current = self.parent.current = "sign_in_screen"
-        self.parent.transition = SlideTransition(direction="left")
