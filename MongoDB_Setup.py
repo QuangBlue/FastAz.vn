70a4b9e72a68e2f39444b8e5acab40a05445df2a
HuyNguyen <huy8208@gmail.com>
2021-01-20 23:49:46 -0800

    Adding User Class and Product Class

--- MongoDB_Setup.py
+++ MongoDB_Setup.py
@@ -1,7 +1,8 @@
 import pymongo
 import datetime
 
-client = pymongo.MongoClient("mongodb+srv://huy8208:741456963@shopeemastertoolcluster.n4haz.mongodb.net/Shopee_Master_Tool_Database?retryWrites=true&w=majority")
+client = pymongo.MongoClient(
+    "mongodb+srv://huy8208:741456963@shopeemastertoolcluster.n4haz.mongodb.net/Shopee_Master_Tool_Database?retryWrites=true&w=majority")
 # client = pymongo.MongoClient("mongodb+srv://quang_db:Thangkhung123@cluster0.cv2te.mongodb.net/shopee_db?retryWrites=true&w=majority")
 
 
@@ -26,30 +27,31 @@ def retrieveDB():
     document = registered_Users_Collection.find({})
     return document
 
-def find_and_updateDB(ids,data):
 
+def find_and_updateDB(ids, data):
     registered_Users_Collection.find_one_and_update(
-        {"_id" : ids},
+        {"_id": ids},
         {"$set": data},
-        upsert=True # Set True se tao moi khi khong co du lieu
+        upsert=True  # Set True se tao moi khi khong co du lieu
     )
 
-def update_one(old_data,new_data): # Hàm này để update theo logic và áp dụng cho update hàng loạt qua hàm for. Từ old_data sẽ tìm và đổi thành giá trị new_data cho kết quả find đầu tiên.
+
+def update_one(old_data,
+               new_data):  # Hàm này để update theo logic và áp dụng cho update hàng loạt qua hàm for. Từ old_data sẽ tìm và đổi thành giá trị new_data cho kết quả find đầu tiên.
 
     registered_Users_Collection.update_one(
         {old_data}, {"$set":
-            {new_data } 
-        }
-    ) 
+                         {new_data}
+                     }
+    )
 
 
-# data = {"_id":"ABC123",'Name': 'Giày China', 'SKU': '4567', 'Image': 'URL'}
-# insertDB(data)
+data = {"_id": "ABC123", 'Name': 'Giày China', 'SKU': '4567', 'Image': 'URL'}
+insertDB(data)
 
 # everything = retrieveDB()
 # for elem in everything:
 #     print(elem)
 # ids = "ABC123"
-# data = {'Name':'hang tau chat luong cao'}
-# updateDB(ids,data)
+# data = {'Name': 'hang tau chat luong cao'}
 
--- userClass.py
+++ userClass.py
@@ -1,5 +1,84 @@
+"""User Class and Product Class used by front-end GUI and backend MongoDB
+    Shopee Data -> User -> Show front-end GUI -> Push to MongoDB
+
+User data structure:
+{"_id": <int>,
+"Username" : <string>,
+"Password" : <string>,
+"Avatar" : <string>,
+"Token" : <string>,
+"List_data_push_product" : {
+        Product_id:{ "Product_name" : <string>, "Product_quantity": <int>, "Product_img": <string>}
+        ...
+    }
+}
+
+Specification:
+Username : indexed, unique
+Token : indexed, unique
+Product_id: indexed, unique
+
+"""
+import json
+
+
+class Product:
+    def __init__(self, productID=None, productName=None, productQuantity=None, productImage=None):
+        try:
+            self._product_id = int(productID)
+            self._product_name = str(productName)
+            self._product_quantity = int(productQuantity)
+            self._product_img = str(productImage)
+        except ValueError as e:
+            print(e)
+
+    def get_product_id(self):
+        return self._product_id
+
+    def get_product_name(self):
+        return self._product_name
+
+    def get_product_quantity(self):
+        return self._product_quantity
+
+    def get_product_img(self):
+        return self._product_img
+
+
 class User:
-    def __init__(self):
-        self.collection = {
-            "ID":
-        }
+    def __init__(self, username=None, password=None, avatar=None, token=None):
+        try:
+            self._username = str(username)
+            self._password = str(password)
+            self._avatar = str(avatar)
+            self._token = str(token)
+            self._new_product = None
+            self._list_data_push_product = dict()
+        except ValueError as e:
+            print(e)
+
+    def __str__(self):
+        """Print out user's object as str"""
+        return "Username: " + str(self._username) + "\n" + "Password: " + self._password + \
+               "\n" + "Avatar: " + self._avatar + "\n" + "Token: " + self._token + \
+               "\n" + str(json.dumps(self._list_data_push_product, indent=4))
+
+    def add_new_product(self, productID, productName, productQuantity, productImage):
+        """ Add sản phẩm mới vào user
+            Ex:
+                user1 = User(username="Huy", password="yes", avatar="img1234", token="token")
+                user1.add_new_product(productID, productName, productQuantity, productImage)
+        """
+        self._new_product = Product(productID, productName, productQuantity, productImage)
+        self._list_data_push_product[self._new_product.get_product_id()] = \
+            {"Product_name": self._new_product.get_product_name(),
+             "Product_quantity": self._new_product.get_product_quantity(),
+             "Product_img": self._new_product.get_product_img()}
+
+
+
+user1 = User(username="Huy", password="yes", avatar="img1234", token="token")
+user1.add_new_product(113,"Balo Nike",100,"Image URL")
+user1.add_new_product(114,"Giay Nike",200,"Image URL")
+
+print(user1)
