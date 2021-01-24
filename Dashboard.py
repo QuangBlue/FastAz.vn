84395810643b1ff0a37c00e2eb0cc4514717bdbe
QuangBlue <quangblue1603@gmail.com>
2021-01-24 15:41:25 +0700

    Update GUI Dashboard

---
+++ Dashboard.py
@@ -0,0 +1,128 @@
+import sys
+import PyQt5.QtCore as QtCore
+import PyQt5.QtGui as QtGui
+from PyQt5.uic import loadUi
+import qrc.file_img_rc
+import webbrowser
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+sys.path.insert(0, 'dashboard/')
+from s_db import *
+  
+
+class Dashboard(QMainWindow):
+    def __init__(self):
+        super(Dashboard,self).__init__()
+        loadUi("ui//dashboard_screen.ui",self)
+        # ########################################################################
+        ## THÊM NUT MENU TẠI ĐÂY
+        # ########################################################################
+        #
+        # B1: Tạo function thêm nút ---> addNewMenu(self, name, objName, icon, isTopMenu):
+        # B2: Thêm nút Menu vào def Button
+        
+            
+        self.stackedWidget.setMinimumWidth(20)
+        UIFunctions.addNewMenu(self, "Open File", "btn_open_file", "url(:/icon/cil-folder-open.png)", True)
+        UIFunctions.addNewMenu(self, "Save", "btn_save", "url(:/icon/cil-save.png)", True)
+        UIFunctions.addNewMenu(self, "New", "btn_new", "url(:/icon/cil-file.png)", True)
+        UIFunctions.addNewMenu(self, "New User", "btn_new_user", "url(:/icon/cil-user-follow.png)", True)
+        UIFunctions.addNewMenu(self, "Setting", "btn_settings", "url(:/icon/cil-settings.png)", False)
+
+
+
+
+        # ########################################################################
+        ## ICON HOẶC AVARTA CỦA USER
+        # ########################################################################
+
+        UIFunctions.userIcon(self, "QH", "", True)
+
+        # ########################################################################
+        ## ẨN CỬA SỔ MẶC ĐỊNH
+        # ########################################################################
+        UIFunctions.removeTitleBar(self,True)
+
+        # ########################################################################
+        ## THIỆT LẬP TÊN CỬA SỔ VÀ THÔNG TIN THÔNG BÁO
+        # ########################################################################
+        self.setWindowTitle('FastAz.vn - Ứng dụng bán hàng chuyên nghiệp')
+        UIFunctions.labelTitle(self, 'FastAz.vn - Ứng dụng bán hàng chuyên nghiệp')
+        UIFunctions.labelDescription(self, 'Mọi câu hỏi hoặc khó khăn vui lòng liên hệ Fanpage FastAz.Vn - Ứng dụng hỗ trợ bán hàng chuyên nghiệp')
+
+        # ########################################################################
+        ## DI CHUYỂN CỬA SỔ
+        # ########################################################################
+        def moveWindow(event):
+            if self.isMaximized() == False:
+                if event.buttons() == Qt.LeftButton:
+                    self.move(self.pos() + event.globalPos() - self.dragPos)
+                    self.dragPos = event.globalPos()
+                    event.accept()
+        self.frame_label_top_btns.mouseMoveEvent = moveWindow
+
+        # ########################################################################
+        ## NÚT TOGGLE
+        # ########################################################################
+        self.btn_toggle_menu.clicked.connect(lambda: UIFunctions.toggleMenu(self, 220, True))
+
+        # ########################################################################
+        ## LỰA CHỌN MENU MẶC ĐỊNH
+        # ########################################################################
+        
+        UIFunctions.selectStandardMenu(self, "btn_open_file")
+        self.stackedWidget.setCurrentWidget(self.page_home)
+
+        # ########################################################################
+        ## HÀM CHẠY CÁC CHỨC NĂNG CỦA CỬA SỔ
+        # ########################################################################
+        UIFunctions.uiDefinitions(self)
+        self.show()
+
+
+    def mousePressEvent(self, event):
+        self.dragPos = event.globalPos()
+
+    def resizeEvent(self, event):
+        return super(Dashboard, self).resizeEvent(event)
+
+    def Button(self):
+        btnWidget = self.sender()
+
+        if btnWidget.objectName() == "btn_open_file":
+            self.stackedWidget.setCurrentWidget(self.page_home)
+            UIFunctions.resetStyle(self, "btn_open_file")
+            UIFunctions.labelPage(self, "Open File")
+            btnWidget.setStyleSheet(UIFunctions.selectMenu(self,btnWidget.styleSheet()))
+
+        if btnWidget.objectName() == "btn_save":
+            self.stackedWidget.setCurrentWidget(self.page_home)
+            UIFunctions.resetStyle(self, "btn_save")
+            UIFunctions.labelPage(self, "Save")
+            btnWidget.setStyleSheet(UIFunctions.selectMenu(self, btnWidget.styleSheet()))
+
+        if btnWidget.objectName() == "btn_new":
+            self.stackedWidget.setCurrentWidget(self.page_home)
+            UIFunctions.resetStyle(self, "btn_new")
+            UIFunctions.labelPage(self, "New")
+            btnWidget.setStyleSheet(UIFunctions.selectMenu(self,btnWidget.styleSheet()))
+
+        if btnWidget.objectName() == "btn_new_user":
+            self.stackedWidget.setCurrentWidget(self.page_home)
+            UIFunctions.resetStyle(self, "btn_new_user")
+            UIFunctions.labelPage(self, "New User")
+            btnWidget.setStyleSheet(UIFunctions.selectMenu(self,btnWidget.styleSheet()))
+
+        if btnWidget.objectName() == "btn_settings":
+            self.stackedWidget.setCurrentWidget(self.page_settings)
+            UIFunctions.resetStyle(self, "btn_settings")
+            UIFunctions.labelPage(self, "Setting")
+            btnWidget.setStyleSheet(UIFunctions.selectMenu(self,btnWidget.styleSheet()))
+    
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    QtGui.QFontDatabase.addApplicationFont('font/Nunito/Nunito-Regular.ttf')
+    QtGui.QFontDatabase.addApplicationFont('font/Nunito/Nunito-Regular.ttf')
+    dashboard = Dashboard()
+    app.exec_() 
---
+++ dashboard/s_db.py
@@ -0,0 +1,194 @@
+from Dashboard import *
+
+GLOBAL_STATE = 0
+GLOBAL_TITLE_BAR = True
+
+count = 1
+
+class UIFunctions(Dashboard):
+    GLOBAL_STATE = 0
+    GLOBAL_TITLE_BAR = True
+
+    def maximize_restore(self):
+        global GLOBAL_STATE
+        status = GLOBAL_STATE
+        if status == 0:
+            self.showMaximized()
+            GLOBAL_STATE = 1
+            self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
+            self.btn_maximize_restore.setToolTip("Restore")
+            self.btn_maximize_restore.setIcon(QtGui.QIcon(u":/icon/cil-window-restore.png"))
+            self.frame_size_grip.hide()
+        else:
+            GLOBAL_STATE = 0
+            self.showNormal()
+            self.resize(self.width()+1, self.height()+1)
+            self.horizontalLayout.setContentsMargins(10, 10, 10, 10)
+            self.btn_maximize_restore.setToolTip("Maximize")
+            self.btn_maximize_restore.setIcon(QtGui.QIcon(u":/icon/cil-window-maximize.png"))
+            self.frame_size_grip.show()
+
+    def removeTitleBar(self, status):
+        global GLOBAL_TITLE_BAR
+        GLOBAL_TITLE_BAR = status
+
+    def labelTitle(self, text):
+        self.label_title_bar_top.setText(text)
+
+    # LABEL DESCRIPTION
+    def labelDescription(self, text):
+        self.label_top_info_1.setText(text)
+
+    def toggleMenu(self, maxWidth, enable):
+        if enable:
+            width = self.frame_left_menu.width()
+            maxExtend = maxWidth
+            standard = 70
+
+            if width == 70:
+                widthExtended = maxExtend
+            else:
+                widthExtended = standard
+
+            self.animation = QPropertyAnimation(self.frame_left_menu, b"minimumWidth")
+            self.animation.setDuration(300)
+            self.animation.setStartValue(width)
+            self.animation.setEndValue(widthExtended)
+            self.animation.setEasingCurve(QtCore.QEasingCurve.InOutQuart)
+            self.animation.start()   
+    
+    def addNewMenu(self, name, objName, icon, isTopMenu):
+        font = QFont()
+        font.setFamily(u"Nunito")
+        font.setPointSize(16)
+        button = QPushButton(str(count),self)
+        button.setObjectName(objName)
+        sizePolicy3 = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+        sizePolicy3.setHorizontalStretch(0)
+        sizePolicy3.setVerticalStretch(0)
+        sizePolicy3.setHeightForWidth(button.sizePolicy().hasHeightForWidth())
+        button.setSizePolicy(sizePolicy3)
+        button.setMinimumSize(QSize(0, 70))
+        button.setLayoutDirection(Qt.LeftToRight)
+        button.setFont(font)
+        button.setStyleSheet(Style.style_bt_standard.replace('ICON_REPLACE', icon))
+        button.setText(name)
+        button.setToolTip(name)
+        button.clicked.connect(self.Button)
+
+        if isTopMenu:
+            self.layout_menus.addWidget(button)
+        else:
+            self.layout_menu_bottom.addWidget(button)
+
+    def selectMenu(self, getStyle):
+        select = getStyle + ("QPushButton { border-right: 7px solid rgb(44, 49, 60); }")
+        return select
+
+    def deselectMenu(self, getStyle):
+        deselect = getStyle.replace("QPushButton { border-right: 7px solid rgb(44, 49, 60); }", "")
+        return deselect
+
+    def selectStandardMenu(self, widget):
+        for w in self.frame_left_menu.findChildren(QPushButton):
+            if w.objectName() == widget:
+                w.setStyleSheet(UIFunctions.selectMenu(self,w.styleSheet()))
+
+    def resetStyle(self, widget):
+        for w in self.frame_left_menu.findChildren(QPushButton):
+            if w.objectName() != widget:
+                w.setStyleSheet(UIFunctions.deselectMenu(self,w.styleSheet()))
+
+    def labelPage(self, text):
+        newText = '| ' + text.upper()
+        self.label_top_info_2.setText(newText)
+
+    def userIcon(self, initialsTooltip, icon, showHide):
+        if showHide:
+            self.label_user_icon.setText(initialsTooltip)
+
+            if icon:
+                style = self.label_user_icon.styleSheet()
+                setIcon = "QLabel { background-image: " + icon + "; }"
+                self.label_user_icon.setStyleSheet(style + setIcon)
+                self.label_user_icon.setText('')
+                self.label_user_icon.setToolTip(initialsTooltip)
+        else:
+            self.label_user_icon.hide()
+
+    def uiDefinitions(self):
+        def dobleClickMaximizeRestore(event):
+            # IF DOUBLE CLICK CHANGE STATUS
+            if event.type() == QtCore.QEvent.MouseButtonDblClick:
+                QtCore.QTimer.singleShot(250, lambda: UIFunctions.maximize_restore(self))
+
+        ## REMOVE ==> STANDARD TITLE BAR
+        if GLOBAL_TITLE_BAR:
+            self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
+            self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+            self.frame_label_top_btns.mouseDoubleClickEvent = dobleClickMaximizeRestore
+        else:
+            self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
+            self.frame_label_top_btns.setContentsMargins(8, 0, 0, 5)
+            self.frame_label_top_btns.setMinimumHeight(42)
+            self.frame_icon_top_bar.hide()
+            self.frame_btns_right.hide()
+            self.frame_size_grip.hide()
+
+
+        ## SHOW ==> DROP SHADOW
+        self.shadow = QGraphicsDropShadowEffect(self)
+        self.shadow.setBlurRadius(17)
+        self.shadow.setXOffset(0)
+        self.shadow.setYOffset(0)
+        self.shadow.setColor(QColor(0, 0, 0, 150))
+        self.frame_main.setGraphicsEffect(self.shadow)
+
+        ## ==> RESIZE WINDOW
+        self.sizegrip = QSizeGrip(self.frame_size_grip)
+        self.sizegrip.setStyleSheet("width: 20px; height: 20px; margin 0px; padding: 0px;")
+
+        ### ==> MINIMIZE
+        self.btn_minimize.clicked.connect(lambda: self.showMinimized())
+
+        ## ==> MAXIMIZE/RESTORE
+        self.btn_maximize_restore.clicked.connect(lambda: UIFunctions.maximize_restore(self))
+
+        ## SHOW ==> CLOSE APPLICATION
+        self.btn_close.clicked.connect(lambda: self.close())
+
+class Style(Dashboard):
+
+    style_bt_standard = (
+    """
+    QPushButton {
+        background-image: ICON_REPLACE;
+        background-position: left center;
+        background-repeat: no-repeat;
+        border: none;
+        border-left: 28px solid rgb(27, 29, 35);
+        background-color: rgb(27, 29, 35);
+        text-align: left;
+        padding-left: 45px;
+    }
+    QPushButton[Active=true] {
+        background-image: ICON_REPLACE;
+        background-position: left center;
+        background-repeat: no-repeat;
+        border: none;
+        border-left: 28px solid rgb(27, 29, 35);
+        border-right: 5px solid rgb(44, 49, 60);
+        background-color: rgb(27, 29, 35);
+        text-align: left;
+        padding-left: 45px;
+    }
+    QPushButton:hover {
+        background-color: rgb(33, 37, 43);
+        border-left: 28px solid rgb(33, 37, 43);
+    }
+    QPushButton:pressed {
+        background-color: rgb(85, 170, 255);
+        border-left: 28px solid rgb(85, 170, 255);
+    }
+    """
+    )
